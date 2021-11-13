a5e180a38c6f48e1d99a4b462abfb5877ab17abe
brahianpdev <zk.spywn@gmail.com>
2021-11-13 16:55:39 -0300

    update: correct path from pokemons

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,9 @@
 const request = require('request');
 
 // Pokemon API
-const custom_URL = 'https://pokeapi.co/api/v2/';
+const custom_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
-export const data = request.get(custom_URL, (error, response, body ) => {
+const data = request.get(custom_URL, (error, response, body ) => {
     if ( error ) {
         return console.dir(error);
     } console.dir(JSON.parse(body));
