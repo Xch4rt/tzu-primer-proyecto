2055e4824d521e8ccdc331a26f640c4103cae9df
brahianpdev <zk.spywn@gmail.com>
2021-11-13 16:38:13 -0300

    feat: data from pokemon api

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+// Pokemon API
+const custom_URL = 'https://pokeapi.co/api/v2/';
+
+export const data = request.get(custom_URL, (error, response, body ) => {
+    if ( error ) {
+        return console.dir(error);
+    } console.dir(JSON.parse(body));
+})
