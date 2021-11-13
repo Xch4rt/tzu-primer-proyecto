4170db70106056c17caec6f6e1abbd70ad7605ce
brahianpdev <zk.spywn@gmail.com>
2021-11-13 17:32:46 -0300

    add: route to get Pokemons

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
+const { getPokemons } = require('../controllers/user');
 
 const router = Router();
 
 // Endpoints here:
+router.get('/pokemon', getPokemons);
+// router.get('/pokemon/:id', getPokemonById)
 
 module.exports = router;
