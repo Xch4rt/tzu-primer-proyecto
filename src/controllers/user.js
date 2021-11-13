f4b3ce5d93f3ab1628190bddb2f5c7a0c4897a7f
brahianpdev <zk.spywn@gmail.com>
2021-11-13 17:32:09 -0300

    add: getPokemons

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,22 @@
-// Here we can use querys
 const { response, request } = require('express');
+const data = require('../data/data');
 
+const getPokemons = (req = request, res = response) => {
+
+    res.json({
+        msg: 'Peticion GET a mi API',
+        data: data
+    });
+}
+
+const getPokemonById = (req = request, res = response ) => {
+    res.json({
+        msg: 'Peticion GET por ID a mi API',
+        // data: data id 
+    })
+}
+
+module.exports = {
+    getPokemons,
+    getPokemonById
+}
