6b1f6d298a890bf595e80d8b8b91c513bf7f7950
brahianpdev <zk.spywn@gmail.com>
2021-11-14 10:34:38 -0300

    add endpoints with controllers

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
+const { getPokemons, getPokemonsID, getPokemonName } = require('../controllers/user');
 
 const router = Router();
 
 // Endpoints here:
+router.get('/pokemon', getPokemons);
+router.get('/pokemon/:id', getPokemonsID);
+router.get('/pokemon/:name', getPokemonName);
 
 module.exports = router;
