adfe4b0be1f94ef87ac5cdc60adce4dfe0b3ac02
brahianpdev <zk.spywn@gmail.com>
2021-11-14 10:34:12 -0300

    add controllers to routes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,39 @@
-// Here we can use querys
-const { response, request } = require('express');
+const request = require('request');
 
+const getPokemons = (req, res) => {
+	let url = `http://pokeapi.co/api/v2/pokemon/`;
+	request(url, (err, resp, body) => {
+		if(!err && resp.statusCode == 200){
+			let data = JSON.parse(body);
+            res.json(data);
+		}
+	});
+};
+
+const getPokemonsID = (req, res) => {
+    const { id } = req.params;
+    let url = `http://pokeapi.co/api/v2/pokemon/${id}`;
+	request(url, (err, resp, body) => {
+		if(!err && resp.statusCode == 200){
+			let data = JSON.parse(body);
+            res.json(data);
+		}
+	});
+};
+
+const getPokemonName = (req, res) => {
+    const { name } = req.params;
+    let url = `http://pokeapi.co/api/v2/pokemon/${name}`;
+	request(url, (err, resp, body) => {
+		if(!err && resp.statusCode == 200){
+			let data = JSON.parse(body);
+            res.json(data);
+		}
+	});
+};
+
+module.exports = {
+    getPokemons,
+    getPokemonsID,
+	getPokemonName
+}
